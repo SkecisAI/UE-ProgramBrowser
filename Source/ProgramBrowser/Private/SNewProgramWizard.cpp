01363bd45ca76157a3b774a3c099331ddeadde9c
SkecisAI <skecis@qq.com>
2023-12-29 16:09:29 +0800

    1. add New Program ui; 2. add icons

--- Source/ProgramBrowser/Private/SNewProgramWizard.cpp
+++ Source/ProgramBrowser/Private/SNewProgramWizard.cpp
@@ -7,14 +7,115 @@
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 
+#define LOCTEXT_NAMESPACE "NewProgramWizard"
+
 void SNewProgramWizard::Construct(const FArguments& InArgs)
 {
-	/*
+	NewProgramTemplates.Add(MakeShareable(new FNewProgramTemplate(
+		FName("Blank"), 
+		FString("A blank program with console."),
+		IPluginManager::Get().FindPlugin("ProgramBrowser")->GetBaseDir() / TEXT("Resource/terminal.png"))));
+	NewProgramTemplates.Add(MakeShareable(new FNewProgramTemplate(
+		FName("Slate Window"), 
+		FString("A program with slate UI."),
+		IPluginManager::Get().FindPlugin("ProgramBrowser")->GetBaseDir() / TEXT("Resource/window.png"))));
+	
+	TemplateListView = SNew(SListView<TSharedRef<FNewProgramTemplate>>)
+		.ListItemSource(&NewProgramTemplates)
+		.OnGenerateRow(this, &SNewProgramWizard::OnGenerateTemplateRow);
+	
+	TSharedRef<SVerticalBox> MainContent = SNew(SVerticalBox)
+	+ SVerticalBox::Slot()
+	.Padding(5.0f)
+	.AutoHeight()
+	[
+		SNew(SHorizontalBox)
+
+		+ SHorizontalBox::Slot()
+		.AutoWidth()
+		.Padding(5.0f)
+		[
+			SNew(STextBlock)
+			.Text(LOCTEXT("InstructionsText", "Choose a template to create program."))
+			.AutoWrapText(true)
+		]
+	]
+
+	+ SVerticalBox::Slot()
+	.Padding(5.0f)
+	[
+		TemplateListView.ToSharedRef()
+	]
+
+	+ SVerticalBox::Slot()
+	.HAlign(HAlign_Right)
+	.Padding(10.0f)
+	.AutoHeight()
+	[
+		SNew(SPrimaryButton)
+		.Text(LOCTEXT("CreateProgramText", "Create Program"))
+	];
+	
 	ChildSlot
 	[
 		// Populate the widget
+		MainContent
 	];
-	*/
+	
 }
 
+TSharedRef<ITableRow> SNewProgramWizard::OnGenerateTemplateRow(TSharedRef<FNewProgramTemplate> NewProgramTemplate, const TSharedRef<STableViewBase>& TableViewBase)
+{
+	return SNew(STableRow<TSharedRef<FNewProgramTemplate>>, TableViewBase)
+	[
+		SNew(SBorder)
+		.BorderImage(FAppStyle::Get().GetBrush("NoBorder"))
+		.Padding(5.0f)
+		[
+			SNew(SBorder)
+			.BorderImage(FAppStyle::Get().GetBrush("ToolPanel.GroupBorder"))
+			.Padding(5.0f)
+			[
+				SNew(SHorizontalBox)
+
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				.Padding(5.0f)
+				[
+					SNew(SBox)
+					.WidthOverride(70.0f)
+					.HeightOverride(70.0f)
+					[
+						SNew(SBorder)
+						[
+							SNew(SImage)
+							.Image(NewProgramTemplate->IconBrush.Get())
+						]
+					]
+				]
+
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				[
+					SNew(SVerticalBox)
+
+					+ SVerticalBox::Slot()
+					[
+						SNew(STextBlock)
+						.Text(FText::FromName(NewProgramTemplate->Name))
+					]
+
+					+ SVerticalBox::Slot()
+					[
+						SNew(STextBlock)
+						.Text(FText::FromString(NewProgramTemplate->Path))
+					]
+				]
+			]
+		]
+	];
+}
+
+
+#undef LOCTEXT_NAMESPACE
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
--- Source/ProgramBrowser/Public/SNewProgramWizard.h
+++ Source/ProgramBrowser/Public/SNewProgramWizard.h
@@ -5,6 +5,25 @@
 #include "CoreMinimal.h"
 #include "Widgets/SCompoundWidget.h"
 
+struct FNewProgramTemplate
+{
+	FName Name;
+	FString Path;
+	FString IconPath;
+
+	TSharedPtr<FSlateDynamicImageBrush> IconBrush;
+
+	FNewProgramTemplate(const FName& InName, const FString& InPath, const FString& InIconPath)
+		:
+	Name(InName),
+	Path(InPath),
+	IconPath(InIconPath)
+	{
+		IconBrush = MakeShareabel(new FSlateDynamicImageBrush(FName(*IconPath), FVector(128, 128)));
+	}
+};
+
+
 /**
  * 
  */
@@ -19,4 +38,11 @@ public:
 
 	/** Constructs this widget with InArgs */
 	void Construct(const FArguments& InArgs);
+
+	TSharedRef<ITableRow> OnGenerateTemplateRow(TSharedRef<FNewProgramTemplate> NewProgramTemplate, const TSharedRef<STableViewBase>& TableViewBase);
+
+private:
+	TSharedPtr<SListView<TSharedRef<FNewProgramTemplate>>> TemplateListView;
+
+	TArray<TSaredRef<FNewProgramTemplate>> NewProgramTemplates;
 };
